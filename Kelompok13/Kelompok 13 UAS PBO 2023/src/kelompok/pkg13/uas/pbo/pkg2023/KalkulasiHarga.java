4c09c8b39d8e85bf31f6c66dc31f6e2004d0d8db
FRDrops <112362185+FRDrops@users.noreply.github.com>
2023-06-26 13:31:18 +0700

    Update KalkulasiHarga.java

--- Kelompok13/Kelompok 13 UAS PBO 2023/src/kelompok/pkg13/uas/pbo/pkg2023/KalkulasiHarga.java
+++ Kelompok13/Kelompok 13 UAS PBO 2023/src/kelompok/pkg13/uas/pbo/pkg2023/KalkulasiHarga.java
@@ -9,8 +9,30 @@ package kelompok.pkg13.uas.pbo.pkg2023;
  * @author ASUS
  */
 public class KalkulasiHarga {
-    
-    public void harga (){
-        System.out.println("Heelo world");
+    int var1;
+    int var2;
+    int var3;
+    int a;
+    int b;
+    int c;
+    int d;
+    int akg;
+    int bkg;
+    int ckg;
+    int TotalU;
+    void JumlahRoti(){
+        a = akg/50;
+        b = bkg/400;
+        c = ckg/190;
+        TotalU = a + b + c;
+    }
+    void TotalHargaU(){
+       a = a*140/100;
+       b = b*140/100;
+       c = c*140/100;
+       TotalU = a + b + c;
+    }
+    void Modal(){
+        d = a*var1 + b*var2 + c*var3;
     }
 }
